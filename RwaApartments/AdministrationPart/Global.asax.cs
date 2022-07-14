546a6bfe69bf6fe7a5c3628206fee859175ed8ea
Asim Ribo <asim.ribo21@gmail.com>
2022-07-14 09:03:54 +0200

    Add friendly URL

--- RwaApartments/AdministrationPart/Global.asax.cs
+++ RwaApartments/AdministrationPart/Global.asax.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
 
@@ -20,6 +21,7 @@ namespace AdministrationPart
         protected void Application_Start(object sender, EventArgs e)
         {
             Application["database"] = repository;
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
 
         protected void Application_Error(object sender, EventArgs e)
